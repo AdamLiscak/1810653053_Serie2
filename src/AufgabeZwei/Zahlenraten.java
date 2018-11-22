b7138280e4df1fc4313f541aa07e291449ebdf7d
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-11-22 11:43:13 +0100

    Aufgabe3

--- src/AufgabeZwei/Zahlenraten.java
+++ src/AufgabeZwei/Zahlenraten.java
@@ -1,5 +1,6 @@
 package AufgabeZwei;
 
+import javax.swing.*;
 import java.util.Random;
 
 public class Zahlenraten
@@ -11,15 +12,53 @@ public class Zahlenraten
         Random rnd = new Random();
         int zufallszahl = rnd.nextInt(max - min + 1) + min;
         // es gibt eine Zufallszahl Zw. 1 und 10
-        System.out.println(zufallszahl);
-       if (zufallszahl%2== 0)
+       /* if (zufallszahl%2== 0)
        {
            System.out.println("diese Zahl ist gerade");
        }
        else
        {
            System.out.println("diese Zahl ist ungerade");
+       } */
+
+        while(true)
+       {
+               String benutzerstring = JOptionPane.showInputDialog("bitte geben sie eine Zahl zwischen " +min+" und "+max+" ein");
+
+           int benutzerzahl = Integer.parseInt(benutzerstring);
+
+           if (benutzerzahl>max||benutzerzahl<min)
+           {
+               continue;
+           }
+
+           else
+           {
+               System.out.println(benutzerzahl);
+               if (zufallszahl==benutzerzahl)
+               {
+                   System.out.println("die zahlen stimmen überein");
+               }
+               else
+               {
+                   System.out.println("die zahlen stimmen nicht überein");
+               }
+               if (benutzerzahl-zufallszahl>2||benutzerzahl-zufallszahl<-2)
+               {
+                   System.out.println("der Betrag der Zahlendifferenz überschreitet 2 ");
+               }
+               else
+               {
+                   System.out.println("Der Betrag der Differenz der Zahlen ist kleiner oder gleich 2");
+               }
+               break;
+           }
+
        }
+
+
+
     }
+
 
 }
